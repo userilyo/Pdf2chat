e2826ddda5ad430f981cf8da645727662171c584
Ilyas A <92877864+userilyo@users.noreply.github.com>
2024-03-25 21:53:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,103 @@
-from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
 from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-import logging
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
+
 
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Ask about your PDF")
-    st.header("Ask about your PDF 💬")
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
 
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    user_question = st.text_input("Ask a Question from the PDF Files")
 
-    # extract the text
-    if pdf is not None:
-        try:
-            pdf_reader = PdfReader(pdf)
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
+    if user_question:
+        user_input(user_question)
 
-            # split into chunks
-            text_splitter = CharacterTextSplitter(
-                separator="\n",
-                chunk_size=4036,
-                chunk_overlap=200,
-                length_function=len
-            )
-            chunks = text_splitter.split_text(text)
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
 
-            # create embeddings
-            embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
-            knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-            # show user input
-            user_question = st.text_input("Ask a question about your PDF:")
-            if user_question:
-                docs = knowledge_base.similarity_search(user_question)
-                llm = OpenAI(model="gpt-3.5-turbo-instruct", openai_api_key=openai_api_key)
-                chain = load_qa_chain(llm, chain_type="stuff")
-                response = chain.run(input_documents=docs, question=user_question)
-                st.write(response)
-        except Exception as e:
-            logging.error(f"An error occurred: {e}")
-            st.error("An error occurred while processing your request.")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
